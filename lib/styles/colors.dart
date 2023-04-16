f4f2e8e37cf2d6a5065dbae4579d49e66a552efa
andrew21-mch <nfonandrew73@gmail.com>
2023-04-16 21:39:44 +0100

    force color

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 Color primaryColor = const Color(0xFF0C9869);
-Color ilocateYellow = Color.fromARGB(251, 116, 63, 176);
-Color ilocateAmber = Color.fromARGB(255, 214, 191, 233);
+Color ilocateYellow = const Color.fromARGB(250, 47, 6, 80);
+Color ilocateAmber = const Color.fromARGB(255, 94, 22, 153);
 Color ilocateWhite = const Color(0xFFFFFFFF);
 Color ilocateBlue = const Color(0xFF3B3B98);
